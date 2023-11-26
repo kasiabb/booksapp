dfe50d538b3616e6eeb2cf0d75f1ea30592d6ec6
Katarzyna Bogusz <kabogusz@interia.pl>
2023-11-26 19:23:42 +0100

    exerciseOOP

--- src/js/script.js
+++ src/js/script.js
@@ -1,96 +1,76 @@
-{
-  ('use strict');
+('use strict');
+class BooksList {
+  constructor() {
+    this.data = [];
+    this.favoriteBooks = [];
+    this.filters = [];
+    this.initData();
+    this.getElements();
+    this.initActions();
+    this.render();
+  }
 
-  const select = {
-    templateOf: {
-      book: '#template-book',
-    },
-    containerOf: {
-      bookList: '.books-panel .books-list',
-      bookImage: '.book__image',
-      bookFilter: '.filters',
-      ratings: '.book__rating__fill',
-    },
-  };
+  initData() {
+    this.data = dataSource.books;
+  }
+  getElements() {
+    this.listOfBooks = document.querySelector('.books-panel .books-list');
+    this.filterOfBooks = document.querySelector('.filters');
+  }
 
-  const templates = {
-    booksList: Handlebars.compile(
-      document.querySelector('#template-book').innerHTML
-    ),
-  };
+  initActions() {
+    this.listOfBooks.addEventListener(
+      'dblclick',
+      this.handleBookDoubleClick.bind(this)
+    );
+    this.filterOfBooks.addEventListener(
+      'click',
+      this.handleFilterClick.bind(this)
+    );
+  }
 
-  function render() {
-    const listOfBook = document.querySelector(select.containerOf.bookList);
-
-    for (let book of dataSource.books) {
-      const ratingBgc = determineRatingBgc(book.rating);
-      const ratingWidth = determineRatingWidth(book.rating);
-
-      const generatedHTML = templates.booksList({
-        name: book.name,
-        price: book.price,
-        image: book.image,
-        rating: book.rating,
-        id: book.id,
-      });
-
-      const elem = utils.createDOMFromHTML(generatedHTML);
-      listOfBook.appendChild(elem);
-
-      const ratingFill = elem.querySelector(select.containerOf.ratings);
-      ratingFill.style.width = ratingWidth;
-      ratingFill.style.background = ratingBgc;
+  handleBookDoubleClick(event) {
+    event.preventDefault();
+    if (event.target.offsetParent.classList.contains('book__image')) {
+      const link = event.target.offsetParent;
+      const bookId = link.getAttribute('data-id');
+      if (this.favoriteBooks.includes(bookId)) {
+        const bookIndex = this.favoriteBooks.indexOf(bookId);
+        link.classList.remove('favorite');
+        this.favoriteBooks.splice(bookIndex, 1);
+      } else {
+        link.classList.add('favorite');
+        this.favoriteBooks.push(bookId);
+      }
     }
   }
-  const favoriteBooks = [];
-  console.log(favoriteBooks);
 
-  function initActions() {
-    const listOfBook = document.querySelector(select.containerOf.bookList);
-    listOfBook.addEventListener('dblclick', function (event) {
-      event.preventDefault();
-      if (event.target.offsetParent.classList.contains('book__image')) {
-        const link = event.target.offsetParent;
-        const bookId = link.getAttribute('data-id');
-        if (favoriteBooks.includes(bookId)) {
-          const bookIndex = favoriteBooks.indexOf(bookId);
-          link.classList.remove('favorite');
-          favoriteBooks.splice(bookIndex, 1);
-        } else {
-          link.classList.add('favorite');
-          favoriteBooks.push(bookId);
-        }
-      }
-    });
-    const filterOfBooks = document.querySelector(select.containerOf.bookFilter);
-    filterOfBooks.addEventListener('click', function (callback) {
-      const clickedElement = callback.target;
-      console.log(clickedElement.value);
+  handleFilterClick(event) {
+    const clickedElement = event.target;
 
-      if (
-        clickedElement.tagName == 'INPUT' &&
-        clickedElement.type == 'checkbox' &&
-        clickedElement.name == 'filter'
-      ) {
-        if (clickedElement.checked) {
-          filters.push(clickedElement.value);
-        } else {
-          const indexOfFilter = filters.indexOf(clickedElement.value);
-          filters.splice(indexOfFilter, 1);
-        }
+    if (
+      clickedElement.tagName == 'INPUT' &&
+      clickedElement.type == 'checkbox' &&
+      clickedElement.name == 'filter'
+    ) {
+      if (clickedElement.checked) {
+        this.filters.push(clickedElement.value);
+      } else {
+        const indexOfFilter = this.filters.indexOf(clickedElement.value);
+        this.filters.splice(indexOfFilter, 1);
       }
-      filterBooks();
-    });
+    }
+    this.filterBooks();
   }
-  const filters = [];
-  function filterBooks() {
-    for (let book of dataSource.books) {
+
+  filterBooks() {
+    for (let book of this.data) {
       let shouldBeHidden = false;
       const filterOfHiddenBooks = document.querySelector(
-        `${select.containerOf.bookImage}[data-id="${book.id}"]`
+        `.book__image[data-id="${book.id}"]`
       );
 
-      for (const filter of filters) {
+      for (const filter of this.filters) {
         if (!book.details[filter]) {
           shouldBeHidden = true;
           break;
@@ -104,7 +84,8 @@
       }
     }
   }
-  function determineRatingBgc(rating) {
+
+  determineRatingBgc(rating) {
     if (rating < 6) {
       return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
     } else if (rating < 8) {
@@ -115,10 +96,44 @@
       return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
     }
   }
-
-  function determineRatingWidth(rating) {
-    return rating * 10 + '%';
+  generateBookHTML(bookData) {
+    return `
+      <div class="book">
+        <div class="book__image" data-id="${bookData.id}">
+          <img src="${bookData.image}" alt="${bookData.name}">
+        </div>
+        <div class="book__info">
+          <h2>${bookData.name}</h2>
+          <p class="book__price">${bookData.price}</p>
+          <div class="book__rating" style="background: ${bookData.ratingBgc}">
+            Rating: ${bookData.rating}
+          </div>
+        </div>
+      </div>
+    `;
   }
-  render();
-  initActions();
+  render() {
+    for (let book of this.data) {
+      const ratingBgc = this.determineRatingBgc(book.rating);
+      const generatedHTML = this.generateBookHTML({
+        name: book.name,
+        price: book.price,
+        image: book.image,
+        rating: book.rating,
+        id: book.id,
+        ratingBgc: ratingBgc,
+      });
+
+      const elem = this.createDOMFromHTML(generatedHTML);
+      this.listOfBooks.appendChild(elem);
+    }
+  }
+
+  createDOMFromHTML(htmlString) {
+    const div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
+  }
 }
+
+new BooksList();
